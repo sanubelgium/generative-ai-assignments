0acdac302c55a917de77b8903901c6d7d9eda549
Santanu <santanu.ss.sahu@gmail.com>
2026-02-04 23:18:46 +0100

    refractored the code

--- assignment/assignment_9/numpy_assignment.py
+++ assignment/assignment_9/numpy_assignment.py
@@ -2,9 +2,9 @@ import numpy as np
 
 ## Task 1
 print("######################## Task 1 ########################")
-OneDArray = np.array([1,2,3,4,5,6,7,8,9,10])
-TwoDArray = np.array([[1,2,3],[4,5,6],[7,8,9]])
-numArray = np.array([10,20,30,40,50])
+OneDArray = np.arange(1,11,dtype=np.int64)
+TwoDArray = np.array(np.arange(1,10,dtype=np.int64).reshape(3,3))
+numArray = np.linspace(10,50,5,dtype=np.int64)
 
 print("1D Array:",OneDArray)
 print("Shape of 1D Array:",OneDArray.shape)
@@ -20,8 +20,8 @@ print("Data type of Num Array:",numArray.dtype)
 
 ## Task 2
 print("######################## Task 2 ########################")
-A = np.array([10, 20, 30, 40, 50])
-B = np.array([1, 2, 3, 4, 5])
+A = np.array([10, 20, 30, 40, 50],dtype=np.int64)
+B = np.array([1, 2, 3, 4, 5],dtype=np.int64)
 
 print("Addition:", A+B)
 print("Subtraction:", A-B)
@@ -31,7 +31,7 @@ print("Power:", A**2)
 
 ## Task 3
 print("######################## Task 3 ########################")
-values = np.array([2,4,6,8,10])
+values = np.array([2,4,6,8,10],dtype=np.int64)
 print("Square root:", np.sqrt(values))
 print("Exponential:", np.exp(values))
 print("Logarithm:", np.log(values))
@@ -40,7 +40,7 @@ print("Cumulative Sum:", np.cumsum(values))
 
 ## Task 4
 print("######################## Task 4 ########################")
-data = np.array([[10,20,30],[40,50,60],[70,80,90]])
+data = np.array([[10,20,30],[40,50,60],[70,80,90]],dtype=np.int64)
 print("Row wise sum:",np.sum(data,axis=1))
 print("Column wise sum:",np.sum(data,axis=0))
 print("Min Value:",np.min(data))
@@ -49,7 +49,7 @@ print("Mean:",np.mean(data))
 
 ##Task 5
 print("######################## Task 5 ########################")
-marks = np.array([78,85,90,66,72,88,95,60])
+marks = np.array([78,85,90,66,72,88,95,60],dtype=np.int64)
 print("Mean:",np.mean(marks))
 print("Median:",np.median(marks))
 print("Variance:",np.var(marks))
@@ -71,7 +71,7 @@ print("Students scored above average:", np.sum(marks > average))
 ##Task 7
 print("######################## Task 7 ########################")
 ##daily sales data
-sales = np.array([1200,1500,900,2000,1800,1700,1600])
+sales = np.array([1200,1500,900,2000,1800,1700,1600],dtype=np.int64)
 
 print("Total weekly sales:",np.sum(sales))
 print("Average daily sales:",np.mean(sales))
